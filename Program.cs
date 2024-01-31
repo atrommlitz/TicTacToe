937a9e0f2332408d64e841662ce049131bf98065
adamtrommlitz <adamtrommlitz3@gmail.com>
2024-01-31 09:29:28 -0700

    New changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Driver()
+    {
+        Console.WriteLine("Welcome to our Tic Tac Toe Game!");
+
+        print
+
+        string[] gameBoard = new string[9];
+        bool gameOver = false;
+        int input = 0;
+
+        for (int i = 0; i < gameBoard.Length; i++)
+        {
+            gameBoard[i] = "_";
+        }
+
+        for (int i = 0; i < gameBoard.Length; i++)
+        {
+            Console.WriteLine("X, it is your turn! Where would you like to go?");
+            input = Console.ReadLine();
+            gameBoard[input] = "X";
+        }
+    }
+}
