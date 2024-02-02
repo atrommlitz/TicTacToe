576b41a42bcae9556f3fe6e3105ea9f442ff6806
conwayhogan <conwayhogan@CONWAYHOGANDB94>
2024-02-01 18:14:24 -0700

    board checkl

--- Support.cs
+++ Support.cs
@@ -38,6 +38,34 @@ namespace TicTacToe
 
         public void GameWinner()
         {
+
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows
+                if (board[i * 3] == board[i * 3 + 1] && board[i * 3 + 1] == board[i * 3 + 2])
+                {
+                    return board[i * 3];
+                }
+
+                // Check columns
+                if (board[i] == board[i + 3] && board[i + 3] == board[i + 6])
+                {
+                    return board[i];
+                }
+            }
+
+            // Check diagonals
+            if (board[0] == board[4] && board[4] == board[8])
+            {
+                return board[0];
+            }
+
+            if (board[2] == board[4] && board[4] == board[6])
+            {
+                return board[2];
+            }
+
+            return -1; // No winner yet
             Console.WriteLine();
 
         }
